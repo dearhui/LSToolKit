808ec1f651e7522211bbc055d23646e843f8164f
dearhui <dearhui@gmail.com>
2023-01-04 11:25:24 +0800

    add avatar view

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "LSToolKit",
+    defaultLocalization: "en",
     platforms: [
         .iOS(.v14)
     ],
@@ -17,13 +18,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI", from: "2.0.0"),
+        .package(url: "https://github.com/CodeSlicing/pure-swift-ui", from: "3.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LSToolKit",
-            dependencies: []),
+            dependencies: ["SDWebImageSwiftUI", .product(name: "PureSwiftUI", package: "pure-swift-ui")]),
         .testTarget(
             name: "LSToolKitTests",
             dependencies: ["LSToolKit"]),
