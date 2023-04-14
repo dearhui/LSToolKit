42ffbad6a9f947391fcb2d6ed27f84e41547b6aa
dearhui <dearhui@gmail.com>
2023-04-14 11:26:10 +0800

    Refactor MaterialDesignTextField for readability

--- Sources/LSToolKit/SwiftUI/Views/MaterialDesignTextField.swift
+++ Sources/LSToolKit/SwiftUI/Views/MaterialDesignTextField.swift
@@ -8,15 +8,24 @@
 import SwiftUI
 import PureSwiftUI
 
-public struct MaterialDesignTextField: View {
+public struct MaterialDesignTextField2: View {
+    @Environment(\.isEnabled) private var isEnabled
+    
+    private let name: LocalizedStringKey
+    private let hint: LocalizedStringKey?
+    @Binding private var value: String
+    private let isSecure: Bool
+    private let verified: Bool
+    private let focused: Bool
+    
+    @State private var mySecure: Bool
     
     public init(name: LocalizedStringKey,
                 value: Binding<String>,
                 isSecure: Bool = false,
                 hint: LocalizedStringKey? = nil,
                 verified: Bool = true,
-                focused: Bool = false)
-    {
+                focused: Bool = false) {
         self.name = name
         self.hint = hint
         self._value = value
@@ -25,82 +34,79 @@ public struct MaterialDesignTextField: View {
         self.focused = focused
         self._mySecure = State(wrappedValue: isSecure)
     }
-    @Environment(\.isEnabled) var isEnabled
     
-    var name: LocalizedStringKey
-    var hint: LocalizedStringKey?
-    @Binding var value: String
-    var isSecure: Bool = false
-    var verified: Bool = true
-    var focused: Bool = false
-    
-    @State private var mySecure: Bool = false
-    
-    var _verified: Bool {
-        if focused {
-            return true
-        }
-        
-        if value.isEmpty {
-            return true
-        }
-        
+    private var _verified: Bool {
+        if focused || value.isEmpty { return true }
         return verified
     }
     
-    var isMini: Bool {
-        return focused || value.isNotEmpty
+    private var isMini: Bool {
+        return focused || !value.isEmpty
     }
     
     public var body: some View {
         VStack (spacing: 4) {
-            // field
-            ZStack {
-                Group {
-                    if isSecure && mySecure {
-                        SecureField("", text: $value)
-                            .autocorrectionDisabled(true)
-                            .autocapitalization(.none)
-                    } else {
-                        TextField("", text: $value)
-                            .autocorrectionDisabled(true)
-                            .autocapitalization(.none)
-                    }
-                }
-                .yOffsetIf(isMini, 8)
-                .foregroundColor(isEnabled ? Color(UIColor.label) : Color(UIColor.secondaryLabel))
-                
-                Text(name)
-                    .greedyWidth(.leading)
-                    .font(isMini ? .caption : .body)
-                    .yOffsetIf(isMini, -12)
-                    .foregroundColor(isMini ? .accentColor : .gray500)
-                    .zIndex(-1)
-            }
-            .padding()
-            .background(
-                RoundedRectangle(cornerRadius: 8)
-                    .strokeBorder(_verified ? Color.clear : Color(UIColor.systemPink))
-                    .background(
-                        Color(UIColor.secondarySystemBackground)
-                            .clipShape(RoundedRectangle(cornerRadius: 8))
-                    )
-            )
-            .overlayIf(isSecure, eyeButton, alignment: .trailing)
-            
-            // hint
-            if (focused || !_verified), let hint = hint {
-                Text(hint)
-                    .font(.caption)
-                    .greedyWidth(.leading)
-                    .padding(.horizontal)
-                    .foregroundColor(_verified ? Color(UIColor.secondaryLabel) : Color(UIColor.systemPink))
-            }
+            textField
+            hintView
         }
         .animation(.easeInOut, value: isMini)
     }
     
-    var eyeButton: some View {
+    private var textField: some View {
+        ZStack {
+            inputField
+            label
+        }
+        .padding()
+        .background(fieldBackground)
+        .overlayIf(isSecure, eyeButton, alignment: .trailing)
+    }
+    
+    @ViewBuilder
+    private var inputField: some View {
+        Group {
+            if isSecure && mySecure {
+                SecureField("", text: $value)
+            } else {
+                TextField("", text: $value)
+            }
+        }
+        .autocorrectionDisabled(true)
+        .autocapitalization(.none)
+        .yOffsetIf(isMini, 8)
+        .foregroundColor(isEnabled ? Color(UIColor.label) : Color(UIColor.secondaryLabel))
+    }
+    
+    private var label: some View {
+        Text(name)
+            .greedyWidth(.leading)
+            .font(isMini ? .caption : .body)
+            .yOffsetIf(isMini, -12)
+            .foregroundColor(isMini ? .accentColor : Color(UIColor.placeholderText))
+            .zIndex(-1)
+    }
+    
+    private var fieldBackground: some View {
+        RoundedRectangle(cornerRadius: 8)
+            .strokeBorder(_verified ? Color.clear : Color(UIColor.systemPink))
+            .background(
+                Color(.secondarySystemFill)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            )
+    }
+    
+    @ViewBuilder
+    private var hintView: some View {
+        if (focused || !_verified), let hint = hint {
+            Text(hint)
+                .font(.caption)
+                .greedyWidth(.leading)
+                .padding(.horizontal)
+                .foregroundColor(_verified ? Color(UIColor.secondaryLabel) : Color(UIColor.systemPink))
+        }
+    }
+    
+    private var eyeButton: some View {
         Button {
             mySecure.toggle()
         } label: {
@@ -112,36 +118,38 @@ public struct MaterialDesignTextField: View {
     }
 }
 
+
 struct MaterialDesignTextField_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
-            MaterialDesignTextField(name: "register_nick_name",
-                                    value: .constant(""),
-                                    hint: "register_nick_name_hint",
-                                    verified: true,
-                                    focused: false)
-            MaterialDesignTextField(name: "register_nick_name",
-                                    value: .constant("disabled true"),
-                                    hint: "register_nick_name_hint",
-                                    verified: true,
-                                    focused: false)
+            MaterialDesignTextField2(name: "register_nick_name",
+                                     value: .constant(""),
+                                     hint: "register_nick_name_hint",
+                                     verified: true,
+                                     focused: false)
+            MaterialDesignTextField2(name: "register_nick_name",
+                                     value: .constant("disabled true"),
+                                     hint: "register_nick_name_hint",
+                                     verified: true,
+                                     focused: false)
             .disabled(true)
-            MaterialDesignTextField(name: "register_nick_name",
-                                    value: .constant("123"),
-                                    hint: "register_nick_name_hint",
-                                    verified: true,
-                                    focused: true)
-            MaterialDesignTextField(name: "register_nick_name",
-                                    value: .constant("123"),
-                                    hint: "register_nick_name_hint",
-                                    verified: false,
-                                    focused: false)
-            MaterialDesignTextField(name: "密碼",
-                                    value: .constant("123"), isSecure: true,
-                                    hint: "register_nick_name_hint",
-                                    verified: false,
-                                    focused: false)
+            MaterialDesignTextField2(name: "register_nick_name",
+                                     value: .constant("123"),
+                                     hint: "register_nick_name_hint",
+                                     verified: true,
+                                     focused: true)
+            MaterialDesignTextField2(name: "register_nick_name",
+                                     value: .constant("123"),
+                                     hint: "register_nick_name_hint",
+                                     verified: false,
+                                     focused: false)
+            MaterialDesignTextField2(name: "密碼",
+                                     value: .constant("123"), isSecure: true,
+                                     hint: "register_nick_name_hint",
+                                     verified: false,
+                                     focused: false)
         }
         .padding()
+//        .backgroundColor(.secondarySystemBackground)
     }
 }
